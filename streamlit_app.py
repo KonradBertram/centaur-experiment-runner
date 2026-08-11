c647c8642c2df0e56b0adadf282a03c6962db8ea
KonradBertram <konrad.bertram@gmx.de>
2026-08-11 13:05:46 +0000

    Ship Batch 1 v2

--- streamlit_app.py
+++ streamlit_app.py
@@ -862,7 +862,7 @@ def render_results(run_data, config):
 
 
 st.caption(
-    "Updated by KB, 11.08.2026, 14.00 CET"
+    "Updated by KB, 11.08.2026, 15.00 CET"
 )
 st.title("Centaur Experiment Runner")
 st.write(
